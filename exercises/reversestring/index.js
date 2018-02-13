3e139328adcab4619595bfadbca424f67e9646d3
Alfredo <atakori@outlook.com>
2018-02-13 00:49:04 -0500

    finished reverseString exercises

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,28 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+/*	Solution #1 let strArray= str.split('').reverse()
+	return strArray.join('')
+*/
+	/*Solution#2*/
+/*	let newArray= [];
+	let strArray= str.split('');
+	for(let i=strArray.length -1; i>= 0; i--) {
+		newArray.push(strArray[i])
+	}
+	return newArray.join('')*/
+
+	/*Solution#3*/
+/*	let reversedStr= '';
+	for(let character of str) {
+		reversedStr = character + reversedStr;
+	}
+	return reversedStr;*/
+
+	/*Solution#4*/
+	return str.split('').reduce((rev, char) => char + rev
+	, '')
+}
 
 module.exports = reverse;
