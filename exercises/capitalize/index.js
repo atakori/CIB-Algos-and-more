214c059e45cfe7972417a50c58bf8054622174b4
Alfredo <atakori@outlook.com>
2018-02-20 00:20:48 -0500

    added alternate capitalize solution

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -14,7 +14,19 @@ function capitalize(str) {
   		let capWord= word[0].toUpperCase() + word.slice(1)
     	capitalizedWords.push(capWord)
   }
-  return capitalizedWords.join(' ')
+  	return capitalizedWords.join(' ')
+
+  	//Solution #2 (looks based on previous character)
+/*  	let result= str[0].toUpperCase();
+
+  	for(let i=1; i<str.length; i++) {
+  		if(str[i -1] === ' ') {
+  			result+= str[i].toUpperCase();
+  		} else {
+  			result+= str[i]
+  		}
+  	}
+  	return result*/
 }
 
 module.exports = capitalize;
