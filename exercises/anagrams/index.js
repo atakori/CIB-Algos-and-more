2bf7c1cacf2096f49d5b002ec4c5cd13d3382617
Alfredo <atakori@outlook.com>
2018-02-19 23:08:49 -0500

    added anagram solution

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,61 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+/*  let charMapA= {}
+  let charMapB= {}
+
+  let str1= strA.replace(/[^\w]/g, "");
+  let str2= strB.replace(/[^\w]/g, "");
+  
+  for(let char of str1) {
+  	if(charMapA[char]) {
+    	charMapA[char]++
+    } else {
+    	charMapA[char]= 1
+    }
+  }
+  
+  for(let char of str2) {
+  	if(charMapB[char]) {
+    	charMapB[char]++
+    } else {
+    	charMapB[char]= 1
+    }
+  }
+  //made charMaps | now compare them
+  if(str1.length === str2.length && ) {
+  
+  } else {
+  
+  }
+  */
+
+  //Solution #1
+  const aCharMap= buildCharMap(stringA);
+  const bCharMap= buildCharMap(stringB);
+
+  //checking for extra keys
+  if(Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+  	return false
+  } else {
+  	for(let char in aCharMap) {
+  		if(aCharMap[char] !== bCharMap[char]) {
+  			return false
+  		} 
+  	}
+  	return true;
+  }
+
+}
+
+function buildCharMap(str) {
+	const charMap= {};
+
+	for(let char of str.replace(/[^\w]/g, '').toLowerCase()) {
+		charMap[char] ? charMap[char]++ : charMap[char] =1
+	}
+	return charMap;
+}
 
 module.exports = anagrams;
