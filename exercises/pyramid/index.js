21b0de3ab0d6c028bd427f6e992eadb7258eb226
Alfredo <atakori@outlook.com>
2018-02-22 00:59:46 -0500

    added recursive solution for pyramid exercise

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,8 +14,8 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {
-	const midpoint= Math.floor((2*n-1)/2)
+function pyramid(n, i=0, level='') {
+	/*const midpoint= Math.floor((2*n-1)/2)
 	for(let i=0; i<n; i++) {
 		let level= '';
 		for(let column=0; column <n*2-1; column++) {
@@ -24,9 +24,28 @@ function pyramid(n) {
 			} else {
 				level+= ' ';
 			}
+			i=1 col=1 mid= 2
 		}
 		console.log(level)
+	}*/
+	//Solution #2 (recursive)
+	const midpoint= Math.floor((2*n-1) /2)
+
+	if(n === i) {
+		return
 	}
+
+	if((n*2-1) === level.length) {
+		console.log(level);
+		return pyramid(n, i+1);
+	}
+
+	if(midpoint - i <=level.length && midpoint + i >=level.length) {
+		level += '#';
+	} else {
+		level += ' ';
+	}
+	pyramid(n,i, level)
 }
 
 module.exports = pyramid;
