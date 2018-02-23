d37d9bed4890597abac0eea732589ab05786f9ca
Alfredo <atakori@outlook.com>
2018-02-23 00:24:58 -0500

    added vowels exercise solution

--- exercises/vowels/index.js
+++ exercises/vowels/index.js
@@ -7,6 +7,27 @@
 //   vowels('Why do you ask?') --> 4
 //   vowels('Why?') --> 0
 
-function vowels(str) {}
+function vowels(str) {
+	/*str= str.toLowerCase();
+	let arr= str.split('');
+	let vowelCount= 0;
+	for(let char of arr) {
+		if(char === 'a' || char === 'e' || char === 'i' || char === 'o' || char === 'u') {
+			vowelCount++
+		}
+	}
+	return vowelCount;
+*/
+	//Solution #2
+	let vowelCount= 0;
+	const vowelLetters= ['a','e', 'i','o','u'];
+
+	for(let char of str.toLowerCase()) {
+		if(vowelLetters.includes(char)) {
+			vowelCount++
+		}
+	}
+	return vowelCount;
+}
 
 module.exports = vowels;
