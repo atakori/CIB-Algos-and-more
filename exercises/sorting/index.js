766b77b9b581de39acda500cf75b3e9bdb014b9f
Alfredo <atakori@outlook.com>
2018-03-01 22:06:43 -0500

    added mergesort solution

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -10,11 +10,30 @@ function selectionSort(arr) {
 }
 
 function mergeSort(arr) {
+	if(arr.length === 1) {
+		return arr;
+	}
 
+	const center= Math.floor(arr.length /2);
+	const left = arr.slice(0, center);
+	//left does not include arr[center]
+	const right= arr.slice(center/*, arr.length*/);
+
+	return merge(mergeSort(left),mergeSort(right));
 }
 
 function merge(left, right) {
+	const results= [];
+	//check to see if either has values in the arrays
+	while(left.length && right.length) {
+		if(left[0] < right[0]) {
+			results.push(left.shift());
+		} else {
+			results.push(right.shift());
+		}
+	}
 
+	return [...results, ...left, ...right];
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort };
