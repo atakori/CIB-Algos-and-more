164172b228fdd7f73c977934f109ae4014036898
Alfredo <atakori@outlook.com>
2018-03-10 19:33:41 -0500

    added insertFirst solution for linkedList

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,29 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+	constructor(data,next = null){
+		this.data=data;
+		this.next= next
+	}
+}
 
-class LinkedList {}
+class LinkedList {
+	constructor(){
+		this.head= null;
+	}
+
+	insertFirst(data) {
+		let newNode= new Node(data)
+		if(this.head === null) {
+			this.head= newNode;
+		} else {
+			//switch places with the newNode and the "first" node
+			let firstNode = this.head;
+			this.head = newNode;
+			newNode.next = firstNode;
+		}
+	}
+}
 
 module.exports = { Node, LinkedList };
